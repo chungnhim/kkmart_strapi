37350e13db22d230bde6068ab48b750dbc0e1f01
Kevin Tran <humg.thongit@gmail.com>
2021-02-02 00:23:13 +0700

    add rating product api

--- api/product-rating/controllers/product-rating.js
+++ api/product-rating/controllers/product-rating.js
@@ -4,5 +4,64 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const _ = require("lodash");
 
-module.exports = {};
+module.exports = {
+    productRating: async (ctx) => {
+        var userId = 0;
+        if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+            try {
+                const { id, isAdmin = false } = await strapi.plugins[
+                    "users-permissions"
+                ].services.jwt.getToken(ctx);
+                userId = id;
+
+                console.log(`userId`, userId);
+            } catch (err) { }
+        }
+
+        if (userId == 0) {
+            ctx.unauthorized(`You're not logged in!`);
+
+            return;
+        }
+
+        const body = _.assign({}, ctx.request.body, ctx.params);
+
+        var entity = {
+            rating_point: parseFloat(body.rating_point),
+            comment: body.comment,
+            user: userId,
+            product: parseFloat(body.product_id),
+            status: 1
+        };
+
+        var res = await strapi.query('product-rating').create(entity);
+        if (_.isNil(res) && _.isNil(res.id)) {
+            ctx.send({
+                message: "Rating has been failed",
+                success: false
+            });
+
+            return;
+        }
+
+        var productRatings = await strapi.query('product-rating').find({
+            product: parseFloat(body.product_id)
+        });
+
+        var totalRating = _.sumBy(productRatings, "rating_point")
+        var avgRating = Number(parseFloat(totalRating / productRatings.length).toFixed(1));
+        console.log(`totalRating`, avgRating);
+
+        await strapi.query('product').update(
+            { id: parseFloat(body.product_id) },
+            { rating_point: avgRating }
+        );
+
+        ctx.send({
+            message: "Rating has been saved",
+            success: true
+        });
+    }
+};
--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -4,12 +4,10 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
-const { sanitizeEntity } = require("strapi-utils");
 const _ = require("lodash");
-const axios = require("axios");
 
 module.exports = {
-    searchProducts: async(ctx) => {
+    searchProducts: async (ctx) => {
         const queryString = _.assign({}, ctx.request.query, ctx.params);
         const params = _.assign({}, ctx.request.params, ctx.params);
 
@@ -60,7 +58,7 @@ module.exports = {
 
         ctx.send(res);
     },
-    getDetails: async(ctx) => {
+    getDetails: async (ctx) => {
         var userId = 0;
         if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
             try {
