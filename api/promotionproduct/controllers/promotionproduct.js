fd50f4b6ce33a8bac50b3f238ee4bbb1cbe2d4ed
Le Thanh Chung <lethanhchung.na@gmail.com>
2021-02-22 17:00:25 +0700

    add function get list promotion active

--- api/promotionproduct/controllers/promotionproduct.js
+++ api/promotionproduct/controllers/promotionproduct.js
@@ -4,5 +4,54 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
+const _ = require('lodash');
+const axios = require('axios');
 
-module.exports = {};
+const removeAuthorFields = (entity) => {
+    const sanitizedValue = _.omit(entity, ['created_by', 'updated_by', 'user', 'formats', 'promotiontype',
+        'reduction', 'promotionapplytype', 'promotionapplyfor', 'activedate', 'enddate', 'isenddate', 'created_at', 'updated_at', 'numberapply', 'isfreeship',
+        'products'
+    ]);
+    _.forEach(sanitizedValue, (value, key) => {
+        if (_.isArray(value)) {
+            sanitizedValue[key] = value.map(removeAuthorFields);
+        } else if (_.isObject(value)) {
+            if (key == 'created_at' || key == 'updated_at') {
+                if (new Date(value) !== "Invalid Date" && !isNaN(new Date(value))) {
+                    if (value == new Date(value).toISOString()) {
+                        sanitizedValue[key] = value;
+                    }
+                }
+
+            } else {
+                sanitizedValue[key] = removeAuthorFields(value);
+            }
+        }
+    });
+
+    return sanitizedValue;
+};
+
+const formatError = error => [
+    { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
+
+module.exports = {
+    getListPromotionActives: async ctx => {
+        var dateTimeUtcNow = new Date(new Date().toUTCString());
+        var dataQuery = {
+            activedate_lte: dateTimeUtcNow,
+            _or: [
+                [{ isenddate: true }],
+                [{ isenddate: false }, { enddate_gte: dateTimeUtcNow }]
+            ],
+            _sort: "id:asc",
+        };
+        var dataresult = await strapi.query('promotionproduct').find(dataQuery);
+        dataresult = await strapi.services.common.normalizationResponse(
+            dataresult
+        );
+        ctx.send(Object.values(removeAuthorFields(dataresult)));
+    }
+};
