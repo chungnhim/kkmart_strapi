d1d6937608b5e12b8dab8666d230ac7e7644e18c
huongga <nguyenhuong.na2111@gmail.com>
2021-03-14 09:24:41 +0700

    Integrate Happy Pay

--- api/paymenttransact/controllers/paymenttransact.js
+++ api/paymenttransact/controllers/paymenttransact.js
@@ -1,8 +1,32 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
+const _ = require('lodash');
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    createpayment: async ctx => {
+
+        let amt_currency = ctx.request.body.currency;
+        let amt = parseFloat(ctx.request.body.amount);
+        let transdes= ctx.request.body.transdes;
+        let orderId = ctx.request.body.orderId;
+        let walletId =  ctx.request.body.walletId;
+        let transID = "khhfyy";
+        let params = {
+            "TransID": transID,
+
+        }
+        console.log("Go to here");
+        let value = await strapi.services.happypaypaymentservice.createPayment(params);
+        
+ctx.send   ('ok');
+
+    }
+};
