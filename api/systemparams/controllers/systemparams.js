9379210c77aa3880f0cd1a35c232af010f224ce5
huongga <nguyenhuong.na2111@gmail.com>
2021-04-26 00:08:39 +0700

    transaction config

--- api/systemparams/controllers/systemparams.js
+++ api/systemparams/controllers/systemparams.js
@@ -4,27 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const sanitizeEntity = require('strapi-utils');
+const _ = require('lodash');
 
-module.exports = {
-
-    getFullProfileCoinValue: async ctx => {
-        console.log(`come on`);
-        let pmType = await strapi.query("transaction-config").findOne({
-            trxconfigid: "002"
-        });
-        let paymentType = await strapi.services.common.normalizationResponse(pmType, ["created_at", "updated_at", "trxconfigid", "dayeffective", "dayexpired", "isexpired", "monthexpired"]);
-
-        let myramount = 0;
-        // calculate to MYR
-        var exchangerate = await strapi.query('exchangerate').findOne({
-            currencycode: 'K',
-            basecurrencycode: 'MYR',
-        });
-        if (exchangerate) {
-            myramount = parseFloat(paymentType.amount / exchangerate.rate).toFixed(2);
-        }
-        paymentType.myramount = myramount;
-        ctx.send(Object.values(paymentType));
-
-    },
-};
+module.exports = {};
