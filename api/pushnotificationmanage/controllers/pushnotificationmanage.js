f0d491320158e29f38b63df906d624c0a2765aed
huongga <nguyenhuong.na2111@gmail.com>
2021-06-27 00:12:27 +0700

    added cms pushnotification search

--- api/pushnotificationmanage/controllers/pushnotificationmanage.js
+++ api/pushnotificationmanage/controllers/pushnotificationmanage.js
@@ -4,9 +4,11 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const _ = require("lodash");
 
 module.exports = {
 
+    //============> this function temprory do not using
     cmsAddPushNotification: async(ctx) => {
         //params
         //{
@@ -49,10 +51,61 @@ module.exports = {
 
 
     },
-    cmsPushNotification: ctx => {
+    cmsSearchPushNotification: async(ctx) => {
         //params
+        const queryString = _.assign({}, ctx.request.query, ctx.params);
+        const params = _.assign({}, ctx.request.params, ctx.params);
 
-        const params = _.assign({}, ctx.request.body, ctx.params);
+        let pageIndex = 1,
+            pageSize = 10;
+
+        if (!_.isNil(params.page_index) && !_.isNil(params.page_size)) {
+            pageIndex = parseInt(params.page_index);
+            pageSize = parseInt(params.page_size);
+        }
+
+        var dataQuery = {
+            _start: (pageIndex - 1) * pageSize,
+            _limit: pageSize,
+            _sort: "id:desc",
+        };
+
+        if (!_.isNil(queryString.title) && !_.isEmpty(queryString.title)) {
+            dataQuery.titlenotification_contains = queryString.title;
+        }
+
+        if (!_.isNil(queryString.content) && !_.isEmpty(queryString.content)) {
+            dataQuery.notificationcontent_contains = queryString.content;
+        }
+
+        if (!_.isNil(queryString.status) && !_.isEmpty(queryString.status)) {
+            dataQuery.status = queryString.status;
+        }
+        if (!_.isNil(queryString.starttime) && !_.isEmpty(queryString.starttime)) {
+            dataQuery.starttime_gte = queryString.fromdate;
+        }
+        if (!_.isNil(queryString.endtime) && !_.isEmpty(queryString.endtime)) {
+            dataQuery.endtime_lte = queryString.todate;
+        }
+
+
+
+        ////console.log(dataQuery);
+
+        var totalRows = await strapi.query('pushnotificationmanage').count(dataQuery);
+        var entities = await strapi.query("pushnotificationmanage").find(dataQuery);
+
+
+        let productModels = await strapi.services.common.normalizationResponse(
+            entities, ["users", "groupcustomers"]
+        );
+
+        var res = {
+            totalRows,
+            source: _.values(productModels)
+        };
+
+        ctx.send(res);
 
     }
 
